0e6442c60284f19be4cf538f4b8c125824f6269d
Francis Ge <francis@pahdolabs.com>
2025-01-19 02:32:26 -0500

    Removed extra log

--- Assets/Scripts/Gameplay/Actor.cs
+++ Assets/Scripts/Gameplay/Actor.cs
@@ -12,7 +12,6 @@ namespace Kumorikuma.Gameplay {
 
         void Start() {
             _GameDirector.OnActorSpawned?.Invoke(this);
-            Debug.Log("MOOO");
         }
 
         private void OnDestroy() {
